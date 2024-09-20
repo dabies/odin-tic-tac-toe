aad1fc6aa6f2aec9faa91501f42e4c34e925689c
Daniel <dabies11@gmail.com>
2024-09-19 19:52:22 -0700

    Finished basic functionality
    Have implemented the code to start the game and make moves. Need to
    implement logic for win conditions, as well as styling and better user
    experience.

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,31 +1,60 @@
+//cache DOM
+const $playBtn = document.querySelector('.play');
+const $boardBtn = document.querySelectorAll('.square');
+const $banner = document.querySelector('.banner');
+const $playerOneName = document.getElementById('playerOne');
+const $playerTwoName = document.getElementById('playerTwo');
+
+//bind events
+$playBtn.addEventListener('click', startGame);
+function startGame() {
+    $boardBtn.forEach((item) => {
+        item.addEventListener('click', makeMove);
+    });
+}
+
 function createPlayer(name, number) {
-    let token;
+    let marker;
 
     if (number === '1') {
-        token= 'X';
-    } else if (numer === '2') {
-        token= 'O';
+        marker= 'X';
+    } else if (number === '2') {
+        marker= 'O';
     } else {
         alert('Stop trying to hack my program');
     }
 
-    const getToken = () => token;
+    const token = () => marker;
 
-    return {name, getToken};
+    return {name, token};
 }
 
-function render() {
+const counter = (function() {
+    let count = 0;
 
+    const getCount = () => count;
+    const addCount = () => count++;
+
+    return {getCount, addCount}
+})();
+
+const game = (function() {
+    let playerOne = createPlayer($playerOneName.value, '1');
+    let playerTwo = createPlayer($playerTwoName.value, '2');
+
+    return {playerOne, playerTwo}
+})();
+
+function makeMove(event) {
+    if (counter.getCount() % 2 === 0) {
+        event.target.textContent = `${game.playerOne.token()}`;
+        counter.addCount();
+    } else {
+        event.target.textContent = `${game.playerTwo.token()}`;
+        counter.addCount();
+    }
 }
 
-function bindEvents() {
+function declareWinner() {
 
-}
-
-function makeMove(token, square) {
-
-}
-
-function declareWinner(name) {
-    
 }
