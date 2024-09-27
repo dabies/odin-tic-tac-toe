2f7dc2d898679cd5a397e1448731aaf482a1879a
Daniel <dabies11@gmail.com>
2024-09-27 00:13:45 -0700

    Added stop game functionality
    created a function that stops the game after a player has won,
    preventing any further moves from being made.

--- ticTacToe.js
+++ ticTacToe.js
@@ -12,61 +12,67 @@ const boardArray = [];
 $playBtn.addEventListener('click', startGame);
 function startGame() {
     $boardBtn.forEach((item) => {
-        item.addEventListener('click', makeMove);
+        item.addEventListener('click', Player.addMakeMove);
         boardArray.push(item);
     });
     display.onBanner(`${$playerOneName.value}'s turn`)
 }
 
-function createPlayer(name, number) {
-    let marker;
-
-    if (number === '1') {
-        marker= 'X';
-    } else if (number === '2') {
-        marker= 'O';
-    } else {
-        alert('Stop trying to hack my program');
+const Player = (function() {
+    const create = function(name, number) {
+        let marker;
+    
+        if (number === '1') {
+            marker= 'X';
+        } else if (number === '2') {
+            marker= 'O';
+        } else {
+            alert('Stop trying to hack my program');
+        }
+    
+        const token = () => marker;
+    
+        return {name, token};
     }
 
-    const token = () => marker;
+    const counter = (function() {
+        let count = 0;
+    
+        const getCount = () => count;
+        const addCount = () => count++;
+    
+        return {getCount, addCount}
+    })();
 
-    return {name, token};
-}
+    const addMakeMove = (event) => Player.makeMove(event);
 
-const counter = (function() {
-    let count = 0;
+    const makeMove = function(event) {
+        let playerOne = Player.create($playerOneName.value, '1');
+        let playerTwo = Player.create($playerTwoName.value, '2');
+    
+        if (counter.getCount() % 2 === 0) {
+            if (event.target.textContent != '') {
+                display.onBanner('Someone has already made a move in that square.')
+            } else {
+                event.target.textContent = `${playerOne.token()}`;
+                counter.addCount();
+                display.onBanner(`${playerTwo.name}'s turn`);
+                gameBoard.findWinner(boardArray);
+            }
+        } else {
+            if (event.target.textContent != '') {
+                display.onBanner('Someone has already made a move in that square.')
+            } else {
+                event.target.textContent = `${playerTwo.token()}`;
+                counter.addCount();
+                display.onBanner(`${playerOne.name}'s turn`);
+                gameBoard.findWinner(boardArray);
+            }
+        }
+    }
 
-    const getCount = () => count;
-    const addCount = () => count++;
-
-    return {getCount, addCount}
+    return {create, makeMove, addMakeMove}
 })();
-
-function makeMove(event) {
-    let playerOne = createPlayer($playerOneName.value, '1');
-    let playerTwo = createPlayer($playerTwoName.value, '2');
-
-    if (counter.getCount() % 2 === 0) {
-        if (event.target.textContent != '') {
-            display.onBanner('Someone has already made a move in that square.')
-        } else {
-            event.target.textContent = `${playerOne.token()}`;
-            counter.addCount();
-            display.onBanner(`${playerTwo.name}'s turn`);
-            readBoard(boardArray);
-        }
-    } else {
-        if (event.target.textContent != '') {
-            display.onBanner('Someone has already made a move in that square.')
-        } else {
-            event.target.textContent = `${playerTwo.token()}`;
-            counter.addCount();
-            display.onBanner(`${playerOne.name}'s turn`);
-            readBoard(boardArray);
-        }
-    }
-}
 
 const display = (function() {
     const resetBoard = function() {
@@ -79,24 +85,31 @@ const display = (function() {
     return {resetBoard, onBanner}
 })();
 
+const gameBoard = (function() {
 
-
-function stopGame() {
-
-}
-
-function readBoard(array) {
-    if (array[0].textContent === 'X' && array[1].textContent === 'X' && array[2].textContent === 'X' ||
-    array[3].textContent === 'X' && array[4].textContent === 'X' && array[5].textContent === 'X' ||
-    array[6].textContent === 'X' && array[7].textContent === 'X' && array[8].textContent === 'X' ||
-    array[0].textContent === 'X' && array[4].textContent === 'X' && array[8].textContent === 'X' ||
-    array[2].textContent === 'X' && array[4].textContent === 'X' && array[6].textContent === 'X') {
-        display.onBanner(`${playerOne.value} won!`);
-    } else if (array[0].textContent === 'O' && array[1].textContent === 'O' && array[2].textContent === 'O' ||
-    array[3].textContent === 'O' && array[4].textContent === 'O' && array[5].textContent === 'O' ||
-    array[6].textContent === 'O' && array[7].textContent === 'O' && array[8].textContent === 'O' ||
-    array[0].textContent === 'O' && array[4].textContent === 'O' && array[8].textContent === 'O' ||
-    array[2].textContent === 'O' && array[4].textContent === 'O' && array[6].textContent === 'O') {
-        display.onBanner(`${playerTwo.value} won!`)
+    const findWinner = function(array) {
+        if (array[0].textContent === 'X' && array[1].textContent === 'X' && array[2].textContent === 'X' ||
+        array[3].textContent === 'X' && array[4].textContent === 'X' && array[5].textContent === 'X' ||
+        array[6].textContent === 'X' && array[7].textContent === 'X' && array[8].textContent === 'X' ||
+        array[0].textContent === 'X' && array[4].textContent === 'X' && array[8].textContent === 'X' ||
+        array[2].textContent === 'X' && array[4].textContent === 'X' && array[6].textContent === 'X') {
+            display.onBanner(`${playerOne.value} won!`);
+            stopGame();
+        } else if (array[0].textContent === 'O' && array[1].textContent === 'O' && array[2].textContent === 'O' ||
+        array[3].textContent === 'O' && array[4].textContent === 'O' && array[5].textContent === 'O' ||
+        array[6].textContent === 'O' && array[7].textContent === 'O' && array[8].textContent === 'O' ||
+        array[0].textContent === 'O' && array[4].textContent === 'O' && array[8].textContent === 'O' ||
+        array[2].textContent === 'O' && array[4].textContent === 'O' && array[6].textContent === 'O') {
+            display.onBanner(`${playerTwo.value} won!`)
+            stopGame();
+        }
     }
-}
+
+    function stopGame() {
+        $boardBtn.forEach((item) => {
+            item.removeEventListener('click', Player.addMakeMove);
+        });
+    }
+
+    return {findWinner, stopGame}
+})();
